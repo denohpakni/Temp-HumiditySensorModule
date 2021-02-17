8289be44d0476ada2327239e6f0111615a8abc58
denohpakni <denohpakni@gmail.com>
2021-02-17 15:08:48 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+How to use a DHT11 Temperature and Humidity Sensor from Elegoo
+
+first time using custom library
+using a Library specifically designed for these sensors
+* import the whole library folder into the "lib" folder
+
---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+#include <dht_nonblocking.h>
+
+#define my_DHT_SENSOR_TYPE DHT_TYPE_11  //define the sensor type from the library
+
+static const int DHT_SENSOR_PIN = 2;  // where the input pin is connected to.
+
+DHT_nonblocking the_thing_dht_sensor( DHT_SENSOR_PIN, my_DHT_SENSOR_TYPE );
+
+void setup( )
+{
+  Serial.begin( 9600);
+}
+void loop( )
+{
+  float temperature;
+  float humidity;
+
+  if(the_thing_dht_sensor.measure(&temperature, &humidity)){
+    Serial.print( "T = " );
+    Serial.print( temperature, 1 );
+    Serial.print( " deg. C, H = " );
+    Serial.print( humidity, 1 );
+    Serial.println( "%" );
+  }
+}
+
